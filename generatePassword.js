ad5911ace5f272debfc0f66a0e75a0270876c657
Veljko-pop_os <veljkoloncarevicharry@gmail.com>
2021-10-27 15:50:53 +0200

    Added password verification

--- generatePassword.js
+++ generatePassword.js
@@ -2,14 +2,23 @@
 // with 67 alphanumeric and special characters
 // and assigns it to the pass array
 // which is joined into a string at the end
+// If the string doesn't have at least one lowercase, one uppercase,
+// one number and one special character, it generates a new string again.
 function generatePass(){
     document.getElementById("usr").value='';
     let n=15;
     const charactersAvailable = "abcdefghijklmnopqrstuvwxyz#$%&_ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     n=document.getElementById("range").value;
-    let pass=[];
-    for (let i=0; i<n; i++)
-        pass[i]=charactersAvailable[Math.floor(Math.random()*67)+1];
-    pass=pass.join('');
+    let pass;
+    const regex_az = new RegExp("[a-z]");
+    const regex_AZ = new RegExp("[A-Z]");
+    const regex_09 = new RegExp("[0-9]");
+    const regex_special = new RegExp("[#$%&_]");
+    do {
+        pass=[];
+        for (let i=0; i<n; i++)
+            pass[i]=charactersAvailable[Math.floor(Math.random()*67)+1];
+        pass=pass.join('');
+    } while (!(regex_az.test(pass) && regex_AZ.test(pass) && regex_09.test(pass) && regex_special.test(pass)));
     document.getElementById("usr").value=pass;
 }
--- readme.md
+++ readme.md
@@ -1,6 +1,10 @@
 # Lightweight Password Generator
 A very simple password generator written in Javascript. Fully responsive (uses Bootstrap!), so it works flawlessly on smartphones too. <br>
 
+Version: 0.1.1<br>
+<br>
+New in this version: <br>
+* Making sure that the password has at least one of each lower & upper case letters, numbers, and special characters.<br>
+<br>
 Planned features: <br>
-* Making sure that the password has at least one of each lower & upper case letters, numbers, and special characters.
 * Adding a "copy to clipboard" button 
